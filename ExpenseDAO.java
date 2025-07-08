99c0e52ea218c56a161583be5893a091a983d15d
Dineshika <99dinuraja@gmail.com>
2025-07-08 22:35:52 +0530

    add getAllExpenses() method with JDBC

--- ExpenseDAO.java
+++ ExpenseDAO.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
 
 public class ExpenseDAO {
+
+    //1.add expense
+    public void addExpense(String description, double amount, String date){
+        String sql = "INSERT INTO expenses (description,amount,expense_date) VALUES (?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement st = conn.prepareStatement(sql)
+        ) {
+            st.setString(1, description);
+            st.setDouble(2, amount);
+            st.setDate(3, Date.valueOf(date));
+            st.executeUpdate();
+            System.out.println("Expense added!");
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    //2. get all expenses
+    public void getAllExpenses() {
+        String sql = "SELECT * FROM expenses";
+        try ( Connection conn = DBConnection.getConnection();
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery(sql);
+
+        ) {
+            while (rs.next()) {
+                System.out.printf("ID: %d Description: %s Amount: %.2f Date: %s",
+                    rs.getInt("id"),
+                    rs.getString("description"),
+                    rs.getDouble("amount"),
+                    rs.getDate("expense_date")
+                );
+            }
+        }
+
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        
+    }
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Expense Tracker!");
+
+        ExpenseDAO ExpDAO = new ExpenseDAO();
+        ExpDAO.addExpense("Academic fee", 375000.00, "2025-07-01");
+
+        ExpDAO.getAllExpenses();
+    }
 }
